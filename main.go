cca4d4409f6fb11a4abbe13631192554834aafad
David Donaghy <ddonaghy@rapid7.com>
2019-05-02 11:24:40 +0100

    adding pods per messages autoscaling

--- main.go
+++ main.go
@@ -3,11 +3,12 @@ package main
 import (
 	"flag"
 	"time"
+	"math"
 
 	log "github.com/Sirupsen/logrus"
 
-	"github.com/Wattpad/kube-sqs-autoscaler/scale"
-	"github.com/Wattpad/kube-sqs-autoscaler/sqs"
+	"github.com/ddonaghy-r7/kube-sqs-autoscaler/scale"
+	"github.com/ddonaghy-r7/kube-sqs-autoscaler/sqs"
 )
 
 var (
@@ -18,6 +19,8 @@ var (
 	scaleDownMessages   int
 	maxPods             int
 	minPods             int
+	scaleByRatio        bool
+	queuePerPodRatio    int
 	awsRegion           string
 
 	sqsQueueUrl              string
@@ -25,9 +28,10 @@ var (
 	kubernetesNamespace      string
 )
 
-func Run(p *scale.PodAutoScaler, sqs *sqs.SqsClient) {
+func Run(p *scale.PodAutoScaler, sqs *sqs.SqsClient, scaleByRatio bool, queuePerPodRatio int) {
 	lastScaleUpTime := time.Now()
 	lastScaleDownTime := time.Now()
+	lastRescaleTime := time.Now()
 
 	for {
 		select {
@@ -39,32 +43,75 @@ func Run(p *scale.PodAutoScaler, sqs *sqs.SqsClient) {
 					continue
 				}
 
-				if numMessages >= scaleUpMessages {
-					if lastScaleUpTime.Add(scaleUpCoolPeriod).After(time.Now()) {
-						log.Info("Waiting for cool down, skipping scale up ")
+				if scaleByRatio {
+					deployment, err := p.Client.Deployments(p.Namespace).Get(p.Deployment)
+					if err != nil {
+						log.Error("Failed to get deployment from kube server, no re-scale occured")
+						continue
+					}
+					currentReplicas := int(deployment.Spec.Replicas)
+					d := math.Max(float64(numMessages),1) / float64(queuePerPodRatio)
+					newReplicas := int(math.Ceil(d))
+					log.WithFields(log.Fields{
+						"numMessage": numMessages,
+						"d": d,
+						"currentReplicas": currentReplicas,
+						"newReplicas": newReplicas,
+					}).Info("Calculating required pods")
+
+					scaleType := newReplicas - currentReplicas
+					if scaleType > 0 { // scale up
+						if lastRescaleTime.Add(scaleUpCoolPeriod).After(time.Now()) {
+							log.Info("Waiting for cool down, skipping scale up ")
+							continue
+						}
+						log.Info("Scaling up")
+						lastRescaleTime = time.Now()
+					} else if scaleType < 0 { // scale down
+						if lastRescaleTime.Add(scaleDownCoolPeriod).After(time.Now()) {
+							log.Info("Waiting for cool down, skipping scale down ")
+							continue
+						}
+						log.Info("Scaling down")
+						lastRescaleTime = time.Now()
+					} else { // no scale
+						log.Info("No scaling required")
 						continue
 					}
 
-					if err := p.ScaleUp(); err != nil {
-						log.Errorf("Failed scaling up: %v", err)
+					if err := p.ReScaleByRatio(newReplicas); err != nil {
+						log.Errorf("Failed re-scaling: %v", err)
 						continue
 					}
 
-					lastScaleUpTime = time.Now()
-				}
+				} else {
+					if numMessages >= scaleUpMessages {
+						if lastScaleUpTime.Add(scaleUpCoolPeriod).After(time.Now()) {
+							log.Info("Waiting for cool down, skipping scale up ")
+							continue
+						}
 
-				if numMessages <= scaleDownMessages {
-					if lastScaleDownTime.Add(scaleDownCoolPeriod).After(time.Now()) {
-						log.Info("Waiting for cool down, skipping scale down")
-						continue
+						if err := p.ScaleUp(); err != nil {
+							log.Errorf("Failed scaling up: %v", err)
+							continue
+						}
+
+						lastScaleUpTime = time.Now()
 					}
 
-					if err := p.ScaleDown(); err != nil {
-						log.Errorf("Failed scaling down: %v", err)
-						continue
-					}
+					if numMessages <= scaleDownMessages {
+						if lastScaleDownTime.Add(scaleDownCoolPeriod).After(time.Now()) {
+							log.Info("Waiting for cool down, skipping scale down")
+							continue
+						}
 
-					lastScaleDownTime = time.Now()
+						if err := p.ScaleDown(); err != nil {
+							log.Errorf("Failed scaling down: %v", err)
+							continue
+						}
+
+						lastScaleDownTime = time.Now()
+					}
 				}
 			}
 		}
@@ -80,6 +127,8 @@ func main() {
 	flag.IntVar(&scaleDownMessages, "scale-down-messages", 10, "Number of messages required to scale down")
 	flag.IntVar(&maxPods, "max-pods", 5, "Max pods that kube-sqs-autoscaler can scale")
 	flag.IntVar(&minPods, "min-pods", 1, "Min pods that kube-sqs-autoscaler can scale")
+	flag.BoolVar(&scaleByRatio, "scale-by-ratio", false, "Use scale by ratio or not")
+	flag.IntVar(&queuePerPodRatio, "queue-per-pod-ratio", 100, "Queue per pod ratio")
 	flag.StringVar(&awsRegion, "aws-region", "", "Your AWS region")
 
 	flag.StringVar(&sqsQueueUrl, "sqs-queue-url", "", "The sqs queue url")
@@ -92,5 +141,5 @@ func main() {
 	sqs := sqs.NewSqsClient(sqsQueueUrl, awsRegion)
 
 	log.Info("Starting kube-sqs-autoscaler")
-	Run(p, sqs)
+	Run(p, sqs, scaleByRatio, queuePerPodRatio)
 }
